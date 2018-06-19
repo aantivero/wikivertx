623c7dec95f85f2048cab5d985406fedbdcbe607
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-19 17:21:51 -0300

    DB initialization

--- src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
+++ src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
@@ -2,11 +2,48 @@ package com.aantivero.wiki.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
+    private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+    private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+    private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+    private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+    private static final String SQL_ALL_PAGES = "select Name from Pages";
+    private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+    private JDBCClient dbClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
     private Future<Void> prepareDatabase() {
         Future<Void> future = Future.future();
+        dbClient = JDBCClient.createNonShared(vertx, new JsonObject()
+            .put("url", "jdbc:hsqldb:file:db/wiki")
+            .put("driver_class", "org.hsqldb.jdbcDriver")
+            .put("max_pool_size", 30));
+
+        dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                LOGGER.error("Could not open a database connection", ar.cause());
+                future.fail(ar.cause());
+            } else {
+                SQLConnection connection = ar.result();
+                connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+                    connection.close();
+                    if(create.failed()) {
+                        LOGGER.error("Database preparation error", create.cause());
+                        future.fail(create.cause());
+                    } else {
+                        future.complete();
+                    }
+                });
+            }
+        });
         return future;
     }
 
