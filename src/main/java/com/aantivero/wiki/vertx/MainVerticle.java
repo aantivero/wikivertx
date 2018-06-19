c96c4878c545be08d93939ba7d05e9d118116bf1
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-19 17:00:04 -0300

    Otra forma ded trabajar con los futures

--- src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
+++ src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
@@ -17,7 +17,19 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
+        //el startHttpServer no se llama si el prepareDatabase no funciono
         Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
         steps.setHandler(startFuture);
+    }
+
+    public void anotherStart(Future<Void> startFuture) throws Exception {
+        Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+        steps.setHandler(ar -> {
+            if (ar.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(ar.cause());
+            }
+        });
     }
 }
