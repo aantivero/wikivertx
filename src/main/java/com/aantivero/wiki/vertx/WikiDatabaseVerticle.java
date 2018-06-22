9b79c64098ba38155dbd33bd4f1a68e9dc58fd18
alejandroantivero <alejandro.antivero@gmail.com>
2018-06-21 22:29:29 -0300

    Define sql queries

--- src/main/java/com/aantivero/wiki/vertx/WikiDatabaseVerticle.java
+++ src/main/java/com/aantivero/wiki/vertx/WikiDatabaseVerticle.java
@@ -4,6 +4,8 @@ import io.vertx.core.AbstractVerticle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+
 public class WikiDatabaseVerticle extends AbstractVerticle {
 
     public static final String CONFIG_WIKIDB_JDBC_URL = "wikidb.jdbc.url";
@@ -14,6 +16,17 @@ public class WikiDatabaseVerticle extends AbstractVerticle {
     public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WikiDatabaseVerticle.class);
+
+    private enum SqlQuery {
+        CREATE_PAGES_TABLE,
+        ALL_PAGES,
+        GET_PAGE,
+        CREATE_PAGE,
+        SAVE_PAGE,
+        DELETE_PAGE
+    }
+
+    private final HashMap<SqlQuery, String> sqlQueries = new HashMap<>();
 
     
 }
