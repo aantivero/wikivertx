1c2dd5f3b50dc926acc0b9c81f9d341cbfa34070
alejandroantivero <alejandro.antivero@gmail.com>
2018-06-23 17:01:41 -0300

    Create a fetch Page test

--- src/test/java/com/aantivero/wiki/vertx/database/WikiDatabaseVerticleTest.java
+++ src/test/java/com/aantivero/wiki/vertx/database/WikiDatabaseVerticleTest.java
@@ -1,6 +1,8 @@
 package com.aantivero.wiki.vertx.database;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -13,10 +15,37 @@ import org.junit.runner.RunWith;
 public class WikiDatabaseVerticleTest {
 
     private Vertx vertx;
+    private WikiDatabaseService service;
 
     @Before
     public void prepare(TestContext context) throws InterruptedException {
         vertx = Vertx.vertx();
+        JsonObject conf = new JsonObject()
+                .put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_URL, "jdbc:hsqldb:mem:testdb;shutdown=true")
+                .put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE, 4);
+
+        vertx.deployVerticle(new WikiDatabaseVerticle(), new DeploymentOptions().setConfig(conf),
+                context.asyncAssertSuccess(id -> {
+                    service = WikiDatabaseService.createProxy(vertx, WikiDatabaseVerticle.CONFIG_WIKIDB_QUEUE);
+                }));
+    }
+
+    @Test
+    public void crud_operations(TestContext context) {
+        Async async = context.async();
+
+        service.createPage("Test", "Some context", context.asyncAssertSuccess(v1 -> {
+
+            service.fetchPage("Test", context.asyncAssertSuccess(json1 -> {
+                System.out.println(json1);
+                context.assertTrue(json1.getBoolean("found"));
+                context.assertTrue(json1.containsKey("id"));
+                context.assertEquals("Some context", json1.getString("rawContent"));
+                async.complete();
+            }));
+        }));
+
+        async.awaitSuccess(5000);
     }
 
     @After
