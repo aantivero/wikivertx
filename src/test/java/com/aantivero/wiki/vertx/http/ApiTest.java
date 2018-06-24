7ca3a762f2549170c5be7b69fdd0dbd9eabfd80d
alejandroantivero <alejandro.antivero@gmail.com>
2018-06-24 20:15:21 -0300

    Implement the post page test

--- src/test/java/com/aantivero/wiki/vertx/http/ApiTest.java
+++ src/test/java/com/aantivero/wiki/vertx/http/ApiTest.java
@@ -2,14 +2,19 @@ package com.aantivero.wiki.vertx.http;
 
 import com.aantivero.wiki.vertx.database.WikiDatabaseVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
@@ -40,5 +45,32 @@ public class ApiTest {
 	@After
 	public void finish(TestContext context) {
 		vertx.close(context.asyncAssertSuccess());
+	}
+
+	@Test
+	public void play_with_api(TestContext context) {
+		Async async = context.async();
+
+		JsonObject page = new JsonObject()
+			.put("name", "Sample")
+			.put("markdown", "# A Page");
+
+		Future<JsonObject> postRequest = Future.future();
+
+		webClient.post("/api/pages")
+			.as(BodyCodec.jsonObject())
+			.sendJsonObject(page, ar -> {
+				if (ar.succeeded()) {
+					HttpResponse<JsonObject> postResponse = ar.result();
+					postRequest.complete(postResponse.body());
+				} else {
+					context.fail(ar.cause());
+				}
+			});
+
+		postRequest.compose(response -> {
+			context.assertTrue(response.getBoolean("success"));
+			async.complete();
+		}, Future.failedFuture("Oh?"));
 	}
 }
