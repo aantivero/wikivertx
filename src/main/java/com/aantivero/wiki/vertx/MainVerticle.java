ea2f9fa388699351581c43bfa0c01eb49b5db72e
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-26 14:05:54 -0300

    Refactoring to deploy verticles in order

--- src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
+++ src/main/java/com/aantivero/wiki/vertx/MainVerticle.java
@@ -1,40 +1,25 @@
 package com.aantivero.wiki.vertx;
 
-import com.aantivero.wiki.vertx.database.WikiDatabaseVerticle;
-import com.aantivero.wiki.vertx.http.AuthInitializerVerticle;
-import io.vertx.core.AbstractVerticle;
+import io.reactivex.Single;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
+import io.vertx.reactivex.core.AbstractVerticle;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
-        // deploying the verticle an asynchronous operation with a Future, return the identifier
-        Future<String> dbVerticleDeployment = Future.future();
-        // create a verticle instance with new
-        vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment.completer());
+        // rx-deploy-verticle
+        Single<String> dbVerticleDeployment = vertx.rxDeployVerticle(
+                "com.aantivero.wiki.vertx.database.WikiDatabaseVerticle");
 
-        dbVerticleDeployment.compose(id -> {
-            // deploy the auth initialize verticle
-            Future<String> authInitDeplyment = Future.future();
-            vertx.deployVerticle(new AuthInitializerVerticle(), authInitDeplyment);
-            return authInitDeplyment;
-        }).compose(id -> {
-            // sequential composition run one asynchronous operation after the other
-           Future<String> httpVerticleDeployment = Future.future();
-           vertx.deployVerticle(
-                   "com.aantivero.wiki.vertx.http.HttpServerVerticle", // class name as a string
-                   new DeploymentOptions().setInstances(2), // number of instances to deploy
-                   httpVerticleDeployment.completer());
-
-           return httpVerticleDeployment; // return the next future
-        }).setHandler(ar -> { // handler
-            if (ar.succeeded()) {
-                startFuture.complete();
-            } else {
-                startFuture.fail(ar.cause());
-            }
-        });
+        // rx secuential composition
+        dbVerticleDeployment.flatMap(id -> {
+            Single<String> httpVerticleDeployment = vertx.rxDeployVerticle("com.aantivero.wiki.vertx.http.HttpServerVerticle",
+                    new DeploymentOptions().setInstances(2));
+            return httpVerticleDeployment;
+        }).flatMap(id ->
+            vertx.rxDeployVerticle("com.aantivero.wiki.vertx.http.AuthInitializerVerticle")
+        ).subscribe(id -> startFuture.complete(), startFuture::fail);
     }
 }
